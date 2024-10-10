8de3c2685987d92503e9ab122d419731b2099d8f
DmitryKRTV <dmitry.korotayev@gmail.com>
2024-10-10 20:16:00 +0300

    feat: init actions

--- config/storybook/preview.ts
+++ config/storybook/preview.ts
@@ -3,6 +3,7 @@ import { Theme } from '../../src/app/providers/theme';
 import { BrowserRouterDecorator } from '../../src/shared/config/storybook/Decorators/BrowserRouterDecorator';
 import { StyleDecorator } from '../../src/shared/config/storybook/Decorators/StyleDecorator';
 import { ThemeDecorator } from '../../src/shared/config/storybook/Decorators/ThemeDecorator';
+import 'loki/configure-react';
 
 const preview: Preview = {
     parameters: {
