16a430b57661495b90b674363abc879f5f5a3188
DmitryKRTV <dmitry.korotayev@gmail.com>
2024-10-14 11:34:48 +0300

    fix: styles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -11,7 +11,7 @@ export const App = () => {
     const {theme} = useTheme();
 
     return (
-        <div className={classNames(`app ${theme}`, {},[theme])}>
+        <div className={classNames(`app`, {},[])}>
             <Suspense fallback={<PageLoader/>}>
                 <Navbar/>
                 <div className="content-page">
--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,4 +1,3 @@
-import { useTheme } from '@/app/providers/theme';
 import { classNames } from '@/shared/lib/classNames';
 import React, {
     ReactNode, useCallback, useEffect, useRef, useState,
@@ -25,7 +24,6 @@ export const Modal = (props: ModalProps) => {
 
     const [isClosing, setIsClosing] = useState(false);
     const timerRef = useRef<ReturnType<typeof setTimeout>>();
-    const { theme } = useTheme();
 
     const closeHandler = useCallback(() => {
         if (onClose) {
@@ -61,8 +59,7 @@ export const Modal = (props: ModalProps) => {
 
     const mods: Record<string, boolean | undefined> = {
         [cls.opened]: isOpen,
-        [cls.isClosing]: isClosing,
-        [cls[theme]]: true,
+        [cls.isClosing]: isClosing
     };
 
     return (
