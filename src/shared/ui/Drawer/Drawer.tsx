fae790005e6f3b0a47a695feea9df36ead6e7365
DmitryKRTV <dmitry.korotayev@gmail.com>
2024-10-23 16:40:11 +0300

    feat: 84

--- src/shared/ui/Drawer/Drawer.tsx
+++ src/shared/ui/Drawer/Drawer.tsx
@@ -1,6 +1,6 @@
 import { useTheme } from '@/app/providers/theme';
 import { classNames } from '@/shared/lib/classNames/classNames';
-import { useAnimationLibs } from '@/shared/lib/components/AnimationProvider';
+import { AnimationProvider, useAnimationLibs } from '@/shared/lib/components/AnimationProvider';
 import { memo, ReactNode, useCallback, useEffect } from 'react';
 import { Overlay } from '../Overlay/Overlay';
 import { Portal } from '../Portal/Portal';
@@ -94,7 +94,7 @@ export const DrawerContent = memo((props: DrawerProps) => {
     );
 });
 
-export const Drawer = memo((props: DrawerProps) => {
+const DrawerAsync = (props: DrawerProps) => {
     const { isLoaded } = useAnimationLibs();
 
     if (!isLoaded) {
@@ -102,4 +102,12 @@ export const Drawer = memo((props: DrawerProps) => {
     }
 
     return <DrawerContent {...props} />;
-});
+};
+
+export const Drawer = (props: DrawerProps) => {
+    return (
+        <AnimationProvider>
+            <DrawerAsync {...props} />
+        </AnimationProvider>
+    );
+};
