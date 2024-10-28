9d98eb0b88d4689bfd09056f1ac2895c042062b8
DmitryKRTv <dmitry.korotaev@google.com>
2024-10-28 14:56:12 +0300

    feat: 114 Article cypress tests

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,13 +1,14 @@
-import { login } from './commands/login';
-Cypress.Commands.add('login', login);
+import * as articleCommands from './commands/article';
+import * as commentsCommands from './commands/comments';
+import * as commonCommands from './commands/common';
+import * as profileCommands from './commands/profile';
+import * as ratingCommands from './commands/rating';
 
-declare global {
-  namespace Cypress {
-    interface Chainable {
-      login(email?: string, password?: string): Chainable<void>
-    }
-  }
-}
+Cypress.Commands.addAll(commonCommands);
+Cypress.Commands.addAll(profileCommands);
+Cypress.Commands.addAll(articleCommands);
+Cypress.Commands.addAll(commentsCommands);
+Cypress.Commands.addAll(ratingCommands);
 
 export { };
 
